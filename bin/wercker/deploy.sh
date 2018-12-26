128dba9e0a43e6761e9d00cb00f006c2ddc450bd
Yuki Takei <yuki@weseek.co.jp>
2018-12-26 23:36:24 +0900

    modify deploy.sh

--- bin/wercker/deploy.sh
+++ bin/wercker/deploy.sh
@@ -21,6 +21,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:weseek/growi-docs.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f origin master:gh-pages
 
 cd -
