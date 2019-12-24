7a7472a9aaf2dc6060a9cfbda642934cf57a3b00
TsuyoshiSuzukief <tsuyoshi@weseek.co.jp>
2019-12-24 18:50:53 +0900

    gw721-move-docs-page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -294,7 +294,7 @@ module.exports = {
                 '/ja/guide/features/page_operation.md',
                 '/ja/guide/features/emoji.md',
                 '/ja/guide/features/history.md',
-                '/ja/guide/features/group.md',
+                '/ja/guide/features/authority.md',
                 '/ja/guide/features/blockdiag.md',
                 '/ja/guide/features/bootstrap.md',
                 '/ja/guide/features/uml_diagrams.md',
@@ -306,7 +306,6 @@ module.exports = {
               collapsable: false,
               children: [
                 '/ja/guide/tips/hierarchical.md',
-                '/ja/guide/tips/authority.md',
                 '/ja/guide/tips/include_html.md',
                 '/ja/guide/tips/checkbox.md',
                 '/ja/guide/tips/page_linker.md',
@@ -373,6 +372,7 @@ module.exports = {
               collapsable: false,
               children: [
                 '/ja/admin-guide/management-cookbook/line-breaks.md',
+                '/ja/admin-guide/management-cookbook/group.md',
                 '/ja/admin-guide/management-cookbook/aws-s3-bucket-setting.md',
                 '/ja/admin-guide/management-cookbook/ldap.md',
                 '/ja/admin-guide/management-cookbook/active-directory.md',
