f5f96997de84360b04e1704548acab6e71b004f0
Shun Miyazawa <m0zurillex@gmail.com>
2023-04-05 07:49:09 +0000

    cleanup

--- docs/.vuepress/config-help-growi-cloud.js
+++ docs/.vuepress/config-help-growi-cloud.js
@@ -1,11 +1,5 @@
 module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
   title: 'help-growi-cloud',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
 
   // Markdown of build target
   patterns: [
@@ -17,62 +11,16 @@ module.exports = {
     '!(ja|en)/dev/**',
   ],
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
-  head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-  ],
-
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
     repo: '',
     editLinks: false,
     docsDir: 'docs',
     editLinkText: '',
     lastUpdated: false,
-    nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
-      },
-      {
-        text: 'Config',
-        link: '/config/',
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org',
-      },
-    ],
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'Guide',
-          collapsable: false,
-          children: [
-            '',
-            'using-vue',
-          ],
-        },
-      ],
-    },
   },
 
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
   ],
-
 };
