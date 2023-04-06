b3971aa6bb737b1dda2339c7fbc72616a8053976
Shun Miyazawa <m0zurillex@gmail.com>
2023-04-06 06:47:09 +0000

    create header

---
+++ docs/.vuepress/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="header">
+    <header class="navbar d-flex align-items-center justify-content-between py-3 px-4 w-auto">
+      <a class="navbar-brand" href="/" title="Home">
+        <span class="gc-brandlogo-midium py-2 fw-bold gc-text-lato">
+          <img src="/assets/images/growi-cloud-logo.png">
+          <span class="text-dark align-middle">GROWI</span><span class="gc-brandlogo-second-level-domain align-middle">.cloud</span>
+        </span>
+      </a>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+.gc-brandlogo-midium {
+  font-size: 20px;
+}
+
+.gc-brandlogo-second-level-domain {
+  color: #33b0c1;
+}
+
+.gc-text-lato {
+  font-family: Lato;
+}
+
+.gc-brandlogo-midium img {
+  width: auto;
+  height: 40px;
+  margin-right: 12px;
+}
+</style>
--- docs/.vuepress/components/theme/HelpGrowiCloudLayout.vue
+++ docs/.vuepress/components/theme/HelpGrowiCloudLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Header />
     <Content />
   </div>
 </template>
