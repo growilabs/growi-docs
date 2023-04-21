02fa372be4ff478291f0b6c8b6134d79f1573ad1
Shun Miyazawa <m0zurillex@gmail.com>
2023-04-21 09:02:15 +0000

    fix

--- docs/.vuepress/help-growi-cloud-theme/Layout.vue
+++ docs/.vuepress/help-growi-cloud-theme/Layout.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <GcHeader />
+    <Header />
     <!-- TODO: 119321 -->
     <!-- <Content /> -->
     <Top />
-    <GcFooter />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@theme/components/Header.vue'
 import Footer from '@theme/components/Footer.vue'
-import Top from '@theme/components/Home.vue'
+import Top from '@theme/components/Top.vue'
 
 export default {
   components: { Header,Footer, Top },
