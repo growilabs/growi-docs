0470a645b1447c33d351614ec708b114535f872d
Shun Miyazawa <m0zurillex@gmail.com>
2023-04-22 06:45:49 +0000

    fix layout

--- docs/.vuepress/help-growi-cloud-theme/components/Top.vue
+++ docs/.vuepress/help-growi-cloud-theme/components/Top.vue
@@ -1,8 +1,11 @@
 <template>
    <div class="container mt-4">
-      <div class="row">
-        <div class="col-md-6">
-          <h4 class="fw-bold mb-3">共通</h4>
+
+      <!-- ▼ 共通, よくある質問 ▼  -->
+      <div class="mb-5">
+        <div class="row">
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">共通</h4>
             <ul class="gc-page-ul">
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
@@ -12,10 +15,31 @@
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
             </ul>
-        </div>
+          </div>
 
-        <div class="col-md-6">
-          <h4 class="fw-bold mb-3">よくある質問</h4>
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">よくある質問</h4>
+              <ul class="gc-page-ul">
+                <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+                <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+                <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+                <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+                <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+                <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+                <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              </ul>
+          </div>
+        </div>
+      </div>
+
+      <!-- ▼  ユーザーヘルプ ▼  -->
+      <div class="mb-5">
+        <h3 class="fw-bold">ユーザーヘルプ</h3>
+        <hr class="mt-3 mb-4">
+
+        <div class="row mb-5">
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">カテゴリ</h4>
             <ul class="gc-page-ul">
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
@@ -25,16 +49,60 @@
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
             </ul>
+          </div>
+
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">カテゴリ</h4>
+            <ul class="gc-page-ul">
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+            </ul>
+          </div>
         </div>
 
-        <!-- ▼  ユーザーヘルプ ▼  -->
+        <div class="row mb-5">
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">カテゴリ</h4>
+            <ul class="gc-page-ul">
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+            </ul>
+          </div>
 
-        <h3 class="mt-5 fw-bold">ユーザーヘルプ</h3>
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">カテゴリ</h4>
+            <ul class="gc-page-ul">
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <!-- ▼  管理者ヘルプ  ▼  -->
+      <div class="mb-5">
+        <h3 class="fw-bold">管理者ヘルプ</h3>
         <hr class="mt-3 mb-4">
 
-        <div class="col-md-6">
-           <h4 class="fw-bold mb-4">カテゴリ</h4>
-            <ul class="gc-page-ul mb-5">
+        <div class="row mb-5">
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">カテゴリ</h4>
+            <ul class="gc-page-ul">
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
@@ -43,11 +111,11 @@
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
             </ul>
-        </div>
+          </div>
 
-        <div class="col-md-6">
-           <h4 class="fw-bold mb-4">カテゴリ</h4>
-            <ul class="gc-page-ul mb-5">
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">カテゴリ</h4>
+            <ul class="gc-page-ul">
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
@@ -56,11 +124,13 @@
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
             </ul>
+          </div>
         </div>
 
-        <div class="col-md-6">
-           <h4 class="fw-bold mb-4">カテゴリ</h4>
-            <ul class="gc-page-ul mb-5">
+        <div class="row mb-5">
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">カテゴリ</h4>
+            <ul class="gc-page-ul">
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
@@ -69,11 +139,11 @@
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
             </ul>
-        </div>
+          </div>
 
-        <div class="col-md-6">
-           <h4 class="fw-bold mb-4">カテゴリ</h4>
-            <ul class="gc-page-ul mb-5">
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">カテゴリ</h4>
+            <ul class="gc-page-ul">
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
@@ -82,16 +152,19 @@
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
             </ul>
+          </div>
         </div>
+      </div>
 
-        <!-- ▼  管理者ヘルプ  ▼  -->
-
-        <h3 class="mt-5 fw-bold">管理者ヘルプ</h3>
+      <!-- ▼  よくある質問  ▼  -->
+      <div class="mb-3">
+        <h3 class="fw-bold">よくある質問</h3>
         <hr class="mt-3 mb-4">
 
-        <div class="col-md-6">
-           <h4 class="fw-bold mb-4">カテゴリ</h4>
-            <ul class="gc-page-ul mb-5">
+        <div class="row mb-5">
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">カテゴリ</h4>
+            <ul class="gc-page-ul">
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
@@ -100,11 +173,11 @@
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
             </ul>
-        </div>
+          </div>
 
-        <div class="col-md-6">
-           <h4 class="fw-bold mb-4">カテゴリ</h4>
-            <ul class="gc-page-ul mb-5">
+          <div class="col-md-6">
+            <h4 class="fw-bold mb-4">カテゴリ</h4>
+            <ul class="gc-page-ul">
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
@@ -113,63 +186,7 @@
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
               <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
             </ul>
-        </div>
-
-        <div class="col-md-6">
-           <h4 class="fw-bold mb-4">カテゴリ</h4>
-            <ul class="gc-page-ul mb-5">
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-            </ul>
-        </div>
-
-        <div class="col-md-6">
-           <h4 class="fw-bold mb-4">カテゴリ</h4>
-            <ul class="gc-page-ul mb-5">
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-            </ul>
-        </div>
-
-        <!-- ▼  よくある質問  ▼  -->
-
-        <h3 class="mt-5 fw-bold">よくある質問</h3>
-        <hr class="mt-3 mb-4">
-
-        <div class="col-md-6">
-           <h4 class="fw-bold mb-4">カテゴリ</h4>
-            <ul class="gc-page-ul mb-5">
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-            </ul>
-        </div>
-
-        <div class="col-md-6">
-           <h4 class="fw-bold mb-4">カテゴリ</h4>
-            <ul class="gc-page-ul mb-5">
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-              <li><a class="gc-page-link" href="#">記事のタイトルが入ります</a></li>
-            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -183,6 +200,10 @@
 
 .gc-page-link:hover {
   text-decoration: underline;
+}
+
+.gc-page-ul {
+  padding-left: 18px;
 }
 
 .gc-page-ul li {
