1eff98c256ec7a6a7d318eaeb0266159e22131db
Yuki Takei <yuki@weseek.co.jp>
2023-05-09 14:09:40 +0000

    WIP: update 61x.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -167,6 +167,7 @@ module.exports = {
               title: 'Upgrading',
               collapsable: false,
               children: [
+                '/en/admin-guide/upgrading/61x.md',
                 '/en/admin-guide/upgrading/60x.md',
                 '/en/admin-guide/upgrading/51x.md',
                 '/en/admin-guide/upgrading/50x.md',
@@ -431,6 +432,7 @@ module.exports = {
               title: 'アップグレード',
               collapsable: false,
               children: [
+                '/ja/admin-guide/upgrading/61x.md',
                 '/ja/admin-guide/upgrading/60x.md',
                 '/ja/admin-guide/upgrading/51x.md',
                 '/ja/admin-guide/upgrading/50x.md',
