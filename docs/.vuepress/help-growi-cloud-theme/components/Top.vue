0efb25a94f33f9b5c9ffb94223396895956fb762
Shun Miyazawa <m0zurillex@gmail.com>
2023-06-06 15:00:18 +0000

    add todo

--- docs/.vuepress/help-growi-cloud-theme/components/Top.vue
+++ docs/.vuepress/help-growi-cloud-theme/components/Top.vue
@@ -1,6 +1,7 @@
 <template>
    <div class="container mt-4">
 
+    <!-- TODO: i18n categoryName -->
     <PageLinks categoryName="Docs" :pageItems="getPageItems(['tutorial', 'tips', 'feature-introduction'])" />
 
       <!-- ▼ 共通, よくある質問 ▼  -->
@@ -208,6 +209,7 @@ export default {
   },
 
   beforeMount() {
+    // TODO: i18n
     this.topPageItems = resolveTopPageItems(this.$site, 'ja');
   },
 
