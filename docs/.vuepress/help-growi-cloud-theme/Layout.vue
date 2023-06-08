1365de068634ee2d311332076616cecfa15fe0c3
satof3 <satof.design@gmail.com>
2023-06-08 23:04:40 +0900

    refactoring scss

--- docs/.vuepress/help-growi-cloud-theme/Layout.vue
+++ docs/.vuepress/help-growi-cloud-theme/Layout.vue
@@ -7,8 +7,8 @@
       <div v-else class="bg-cloud right-flip py-5">
         <div class="container" id="header">
           <div class="row">
-            <h1 class="col-12 col-md-8 fs-2 border-0 mb-4 mb-md-0 fw-bold text-center text-md-start">
-              <a href="/help/ja" class="text-dark text-decoration-none">
+            <h1 class="col-12 col-md-8 fs-2 border-0 mb-4 mb-md-0 pb-0 fw-bold text-center text-md-start">
+              <a href="/help/ja" class="text-blue-dark text-decoration-none">
                 GROWI.cloud ヘルプページ
               </a>
             </h1>
@@ -24,16 +24,22 @@
         </div>
       </div>
       <div class="cloud-help-bg">
-        <div class="container py-5">
+        <div class="container px-3 px-md-auto py-5">
           <!-- TODO:対応するパンくずリストが表示されるようにする -->
-          <p class="text-muted">
+          <p class="text-muted d-none d-md-block">
             <i class="fa fa-house me-2" />
               <i class="fa fa-angle-right me-2" />
             <a class="me-2 text-decoration-none text-muted">helpTop</a>
             <i class="fa fa-angle-right me-2" />
             <a>page</a>
           </p>
-          <Content />
+            <Content class="article" />
+          <div class="text-center fw-bold return">
+            <a href="/help/ja" class="fs-4 text-decolation-none text-blue-dark">
+              <i class="fa fa-angle-left me-3" />
+              ヘルプ一覧に戻る
+            </a>
+          </div>
         </div>
        </div>
     <Footer />
@@ -62,93 +68,92 @@ export default {
 
 >
 <style lang="scss">
+@import '../styles/style.scss';
 
 $light-blue: #EDF9FF;
 $border-light: #E0E0E0;
-$text-blue-dark: #34495E;
+$text-blue-dark: #34495e;
 $link-text: #00A5B1;
 $code-text: #BD0039;
 $code-border: #abb4bd;
 $code-bg: #eff1f3;
 $light-gray: #f6f8fa;
 
-img {
-  width: 100%;
+.article {
+  img {
+    width: 100%;
+  }
+
+  .header-anchor {
+    display: none;
+  }
+
+  h1,h2,h3,h4,h5,h6 {
+    font-weight: 700;
+    color:$text-blue-dark;
+  }
+
+  h1 {
+    padding-bottom: 8px;
+    margin: 24px 0 36px;
+    border-bottom: solid 1px $border-light;
+  }
+
+  h2 {
+    margin: 48px 0 24px;
+  }
+
+  h3,h4,h5,h6 {
+    margin: 32px 0 16px;
+  }
+
+  h3 {
+    font-size: 24px;
+  }
+
+  h3 {
+    font-size: 20px;
+  }
+
+  li,p {
+    margin-bottom: 0.625em;
+    line-height: 1.6em;
+  }
+
+  a {
+    color: $link-text;
+    text-decoration: none;
+  }
+
+  code {
+    padding: 2px 4px;
+    color: $code-text;
+    background: $code-bg;
+    border: solid 1px $code-border;
+    border-radius: 4px;
+  }
+
+  table {
+    margin: 16px 0;
+  }
+
+  td,th {
+    padding: 8px 16px;
+    border: solid 1px $border-light;
+  }
+
+  th {
+    font-weight: 700;
+    text-align: center;
+  }
+
+  tr:nth-of-type(2n) td {
+    background-color: $light-gray;
+  }
 }
 
-.header-anchor {
-  display: none;
-}
-
-h1,h2,h3,h4,h5,h6 {
-  font-weight: 700;
-  color:$text-blue-dark;
-}
-
-h1 {
-  padding-bottom: 8px;
-  margin: 24px 0 36px;
-  border-bottom: solid 1px $border-light;
-}
-
-h2 {
-  margin: 48px 0 24px;
-}
-
-h3,h4,h5,h6 {
-  margin: 32px 0 16px;
-}
-
-h3 {
-  font-size: 24px;
-}
-
-h3 {
-  font-size: 20px;
-}
-
-li,p {
-  margin-bottom: 0.625em;
-  line-height: 1.6em;
-}
-
-a {
-  color: $link-text;
-  text-decoration: none;
-}
-
-code {
-  padding: 2px 4px;
-  color: $code-text;
-  background: $code-bg;
-  border: solid 1px $code-border;
-  border-radius: 4px;
-}
-
-table {
-  margin: 16px 0;
-}
-
-td,th {
-  padding: 8px 16px;
-  border: solid 1px $border-light;
-}
-
-th {
-  font-weight: 700;
-  text-align: center;
-}
-
-tr:nth-of-type(2n) td {
-  background-color: $light-gray;
-}
-
-.bg-cloud {
-  background-color: $light-blue;
-  background-image: url('/assets/images/cloud-background-abstract-left.png'),url('/assets/images/cloud-background-abstract-right.png');
-  background-repeat: no-repeat;
-  background-position: left -50% bottom -3vw,right -50% bottom -3vw;
-  background-size: 50%;
+.return {
+  margin: 72px 0 56px;
 }
 
 </style>
