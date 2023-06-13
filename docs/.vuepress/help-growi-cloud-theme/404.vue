0ff9a35e6d9947dfdb1de0d73792f38e216d17a0
Shun Miyazawa <m0zurillex@gmail.com>
2023-06-13 03:11:00 +0000

    When navigating to a non-existent page, redirect to the top page

---
+++ docs/.vuepress/help-growi-cloud-theme/404.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+export default {
+  mounted() {
+    if (location.pathname.startsWith('/help/ja/')) {
+      this.$router.push('/ja/');
+    }
+    else {
+      this.$router.push('/en/');
+    }
+  },
+}
+</script>
