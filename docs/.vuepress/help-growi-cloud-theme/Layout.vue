4e79378f3e705c524f4fea70a1721aa03f0a95b2
satof3 <satof.design@gmail.com>
2023-06-13 17:12:24 +0900

    Add vuepress theme-default

--- docs/.vuepress/help-growi-cloud-theme/Layout.vue
+++ docs/.vuepress/help-growi-cloud-theme/Layout.vue
@@ -78,85 +78,85 @@ $code-border: #abb4bd;
 $code-bg: #eff1f3;
 $light-gray: #f6f8fa;
 
-.article {
-  img {
-    width: 100%;
-    margin: 24px 0;
-  }
+// .article {
+//   img {
+//     width: 100%;
+//     margin: 24px 0;
+//   }
 
-  .header-anchor {
-    display: none;
-  }
+//   .header-anchor {
+//     display: none;
+//   }
 
-  h1,h2,h3,h4,h5,h6 {
-    font-weight: 700;
-    color:$text-blue-dark;
-  }
+//   h1,h2,h3,h4,h5,h6 {
+//     font-weight: 700;
+//     color:$text-blue-dark;
+//   }
 
-  h1 {
-    padding-bottom: 8px;
-    margin: 24px 0 24px;
-    border-bottom: solid 1px $border-light;
-  }
+//   h1 {
+//     padding-bottom: 8px;
+//     margin: 24px 0 24px;
+//     border-bottom: solid 1px $border-light;
+//   }
 
-  h2 {
-    margin: 40px 0 24px;
-  }
+//   h2 {
+//     margin: 40px 0 24px;
+//   }
 
-  h3,h4,h5,h6 {
-    margin: 32px 0 16px;
-  }
+//   h3,h4,h5,h6 {
+//     margin: 32px 0 16px;
+//   }
 
-  h3 {
-    font-size: 24px;
-  }
+//   h3 {
+//     font-size: 24px;
+//   }
 
-  h3 {
-    font-size: 20px;
-  }
+//   h3 {
+//     font-size: 20px;
+//   }
 
-  h4 {
-    line-height: 2rem;
-  }
+//   h4 {
+//     line-height: 2rem;
+//   }
 
-  li,p {
-    margin-bottom: 0.625em;
-    line-height: 1.6em;
-  }
+//   li,p {
+//     margin-bottom: 0.625em;
+//     line-height: 1.6em;
+//   }
 
-  a {
-    color: $link-text;
-    text-decoration: none;
-  }
+//   a {
+//     color: $link-text;
+//     text-decoration: none;
+//   }
 
-  code {
-    padding: 2px 4px;
-    color: $code-text;
-    background: $code-bg;
-    border: solid 1px $code-border;
-    border-radius: 4px;
-  }
+// code {
+//   padding: 2px 4px;
+//   color: $code-text;
+//   background: $code-bg;
+//   border: solid 1px $code-border;
+//   border-radius: 4px;
+// }
 
-  table {
-    margin: 16px 0;
-  }
+//   table {
+//     margin: 16px 0;
+//   }
 
-  td,th {
-    padding: 8px 16px;
-    border: solid 1px $border-light;
-  }
+//   td,th {
+//     padding: 8px 16px;
+//     border: solid 1px $border-light;
+//   }
 
-  th {
-    font-weight: 700;
-    text-align: center;
-  }
+//   th {
+//     font-weight: 700;
+//     text-align: center;
+//   }
 
-  tr:nth-of-type(2n) td {
-    background-color: $light-gray;
-  }
-}
+//   tr:nth-of-type(2n) td {
+//     background-color: $light-gray;
+//   }
+// }
 
-.return {
-  margin: 72px 0 56px;
-}
+// .return {
+//   margin: 72px 0 56px;
+// }
 </style>
