389eaad290138bebb62ccc78a858bb9e330943d5
Shun Miyazawa <m0zurillex@gmail.com>
2023-06-19 01:29:53 +0000

    imprv

--- docs/.vuepress/help-growi-cloud-theme/components/Header.vue
+++ docs/.vuepress/help-growi-cloud-theme/components/Header.vue
@@ -38,7 +38,7 @@
         <li class="nav-item p-2 p-md-0 ml-md-3">
           <a
             type="button"
-            class="btn gc-bg-gradation-skyblue text-white fw-bold  mt-md-0 py-md-2 px-md-3"
+            class="btn gc-bg-gradation-skyblue gc-global-menu-button text-white fw-bold  mt-md-0 py-md-2 px-md-3"
             href="https://growi.cloud/signup?contractType=CORPORATION">
               {{ this.$themeLocaleConfig.free_trial }}
             <i class="ml-2 fas fa-angle-right"></i>
@@ -46,11 +46,26 @@
         </li>
 
         <li class="nav-item p-2 p-md-0 ml-md-3">
-          <a href="https://growi.cloud/catalog-request" target="_blank" rel="noopener noreferrer" class="nav-link gc-text-dark fw-bold">{{ this.$themeLocaleConfig.catalog_request }}</a>
+          <!-- SP mode -->
+          <a href="https://growi.cloud/catalog-request"
+             target="_blank"
+             rel="noopener noreferrer"
+             class="btn btn-outline-dark d-block d-md-none gc-global-menu-button fw-bold">
+             {{ this.$themeLocaleConfig.catalog_request }}
+             <i class="ml-2 fas fa-angle-right"></i>
+          </a>
+
+          <!-- PC mode -->
+          <a href="https://growi.cloud/catalog-request"
+             target="_blank"
+             rel="noopener noreferrer"
+             class="gc-global-nav-link d-none d-md-block nav-link gc-text-dark fw-bold">
+            {{ this.$themeLocaleConfig.catalog_request }}
+          </a>
         </li>
 
         <li class="nav-item p-2 p-md-0 ml-md-3">
-          <a href="https://growi.cloud/login" class="nav-link gc-text-dark fw-bold">{{ this.$themeLocaleConfig.login }}</a>
+          <a href="https://growi.cloud/login" class="gc-global-nav-link d-block nav-link gc-text-dark fw-bold text-center">{{ this.$themeLocaleConfig.login }}</a>
         </li>
       </ul>
     </div>
@@ -58,6 +73,10 @@
 </template>
 
 <style lang="scss" scoped>
+@import '~bootstrap/scss/functions';
+@import '~bootstrap/scss/variables';
+@import '~bootstrap/scss/mixins/breakpoints';
+
 .gc-text-dark {
   color: #254459;
 }
@@ -81,4 +100,18 @@
   content: '';
   @extend .gc-bg-gradation-skyblue;
 }
+
+.gc-global-menu-button {
+  -webkit-appearance: none;
+  @include media-breakpoint-down(sm) {
+    width: 100%;
+  }
+}
+
+.gc-global-nav-link {
+  @include media-breakpoint-down(sm) {
+    font-size: 14px;
+  }
+}
+
 </style>
