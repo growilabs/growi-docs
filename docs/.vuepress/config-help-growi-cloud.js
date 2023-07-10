0b497f11162925c51e1d1c51b72ce0c5cadc73be
moekumasaka <moe@weseek.co.jp>
2023-07-10 15:06:09 +0900

    fixed indentation

--- docs/.vuepress/config-help-growi-cloud.js
+++ docs/.vuepress/config-help-growi-cloud.js
@@ -372,17 +372,14 @@ module.exports = {
       searchMaxSuggestions: 10
     }],
     '@vuepress/plugin-back-to-top',
-    [
-      'vuepress-plugin-medium-zoom',
-      {
+    ['vuepress-plugin-medium-zoom', {
         selector: '.article img',
         delay: 1000,
         options: {
           margin: 24,
           scrollOffset: 0,
-        },
-      },
-    ],
+        }
+    }],
     ['container', {
       type: 'tip',
       defaultTitle: {
