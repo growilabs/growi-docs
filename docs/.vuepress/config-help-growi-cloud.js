f09ea2c35cf87cadfa65948368e5bdb97c5d0fd5
aika-h <aika-h@weseek.co.jp>
2023-11-20 07:01:17 +0000

    title rename

--- docs/.vuepress/config-help-growi-cloud.js
+++ docs/.vuepress/config-help-growi-cloud.js
@@ -167,7 +167,7 @@ module.exports = {
           key: 'management-settings',
           children: [
             '/ja/admin-guide/management-cookbook/app-settings.md',
-            '/ja/admin-guide/management-cookbook/line-breaks.md',
+            '/ja/admin-guide/management-cookbook/markdown.md',
             '/ja/admin-guide/management-cookbook/security.md',
             '/ja/admin-guide/management-cookbook/user-management.md',
             '/ja/admin-guide/management-cookbook/group.md',
@@ -312,7 +312,7 @@ module.exports = {
           key: 'management-settings',
           children: [
             '/en/admin-guide/management-cookbook/app-settings.md',
-            '/en/admin-guide/management-cookbook/line-breaks.md',
+            '/en/admin-guide/management-cookbook/markdown.md',
             '/en/admin-guide/management-cookbook/security.md',
             '/en/admin-guide/management-cookbook/user-management.md',
             '/en/admin-guide/management-cookbook/group.md',
