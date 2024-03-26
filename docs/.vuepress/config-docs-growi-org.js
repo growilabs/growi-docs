886d870617042e56ffa5f3c9d04ba428e0b956e2
Yuki Takei <yuki@weseek.co.jp>
2024-03-26 19:00:51 +0000

    update config

--- docs/.vuepress/config-docs-growi-org.js
+++ docs/.vuepress/config-docs-growi-org.js
@@ -186,6 +186,7 @@ module.exports = {
               title: 'Upgrading',
               collapsable: false,
               children: [
+                '/en/admin-guide/upgrading/70x.md',
                 '/en/admin-guide/upgrading/63x.md',
                 '/en/admin-guide/upgrading/62x.md',
                 '/en/admin-guide/upgrading/61x.md',
@@ -434,6 +435,7 @@ module.exports = {
               title: 'アップグレード',
               collapsable: false,
               children: [
+                '/ja/admin-guide/upgrading/70x.md',
                 '/ja/admin-guide/upgrading/63x.md',
                 '/ja/admin-guide/upgrading/62x.md',
                 '/ja/admin-guide/upgrading/61x.md',
--- docs/.vuepress/config-help-growi-cloud.js
+++ docs/.vuepress/config-help-growi-cloud.js
@@ -192,6 +192,7 @@ module.exports = {
           'title': 'アップグレードガイド',
           'key': 'upgrade-guide',
           'children': [
+            '/ja/admin-guide/upgrading/70x.md',
             '/ja/admin-guide/upgrading/63x.md',
             '/ja/admin-guide/upgrading/62x.md',
             '/ja/admin-guide/upgrading/61x.md',
@@ -339,6 +340,7 @@ module.exports = {
           'title': 'Uupgrade guide',
           'key': 'upgrade-guide',
           'children': [
+            '/en/admin-guide/upgrading/70x.md',
             '/en/admin-guide/upgrading/63x.md',
             '/en/admin-guide/upgrading/62x.md',
             '/en/admin-guide/upgrading/61x.md',
