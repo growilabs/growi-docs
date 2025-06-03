134014f0c49c89a9dd8b31984f221bb6986b987f
Yuki Takei <yuki@weseek.co.jp>
2025-06-03 10:18:42 +0000

    fix: remove unused pnpm feature and update postCreateCommand script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,11 @@
 sudo chown -R vscode:vscode /workspace;
 
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Setup pnpm
+SHELL=bash pnpm setup
+eval "$(cat /home/vscode/.bashrc)"
+
+# Install dependencies
 pnpm install
